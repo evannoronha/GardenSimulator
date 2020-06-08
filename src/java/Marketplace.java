a8d4ccf3abcdcc25afdf2aac216cc38140d744cf
Evan W. Noronha <hello@evannoronha.com>
2020-06-08 06:20:16 -0700

    Implement basic list view for marketplace

--- src/java/Marketplace.java
+++ src/java/Marketplace.java
@@ -1,4 +1,9 @@
 
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.support.ConnectionSource;
+import java.sql.SQLException;
+import java.util.List;
 import javax.annotation.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.inject.Named;
@@ -19,5 +24,14 @@ public class Marketplace {
 
     public String visitMarketplace() {
         return "visitMarketplace";
+    }
+
+    public List<MarketListing> getListings() throws SQLException {
+        ConnectionSource cs = DBConnect.getConnectionSource();
+
+        Dao<MarketListing, Integer> listingDao
+                = DaoManager.createDao(cs, MarketListing.class);
+
+        return listingDao.queryForAll();
     }
 }
