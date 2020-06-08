868b81a8ce5316e243341d09ce74d618c36e05b5
Evan W. Noronha <hello@evannoronha.com>
2020-06-08 01:42:16 -0700

    Add placeholders for marketplace

---
+++ src/java/Marketplace.java
@@ -0,0 +1,23 @@
+
+import javax.annotation.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.inject.Named;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Evan
+ */
+@Named(value = "marketplace")
+@SessionScoped
+@ManagedBean
+public class Marketplace {
+
+    public String visitMarketplace() {
+        return "visitMarketplace";
+    }
+}
