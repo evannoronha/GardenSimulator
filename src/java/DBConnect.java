70a7ed55cdc77f2e4d0dd10920b9322035172d24
Evan W. Noronha <hello@evannoronha.com>
2020-06-10 00:49:00 -0700

    remove unused function

--- src/java/DBConnect.java
+++ src/java/DBConnect.java
@@ -1,27 +1,9 @@
 
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnect {
-
-    public Connection getConnection() {
-        Connection connection = null;
-
-        try {
-            connection = DriverManager.getConnection(
-                    "jdbc:postgresql://ambari-node5.csc.calpoly.edu/team1", "team1",
-                    "csc366"); //Was going to make .env for token but private repo so who cares
-
-        } catch (SQLException e) {
-            System.out.println("Connection Failed! Check output console");
-            e.printStackTrace();
-            return null;
-        }
-        return connection;
-    }
 
     public DBConnect() {
         try {
