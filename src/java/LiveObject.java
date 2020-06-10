2bab94957a5be0a6c12dbb42a5e177886cb80b3e
Evan W. Noronha <hello@evannoronha.com>
2020-06-09 23:46:07 -0700

    Add Liveobject class

---
+++ src/java/LiveObject.java
@@ -0,0 +1,21 @@
+
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.support.ConnectionSource;
+import java.sql.SQLException;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Evan
+ */
+abstract class LiveObject<THIS extends LiveObject<THIS>> {
+
+    public Dao<THIS, Object> getDao(ConnectionSource cs) throws SQLException {
+        return DaoManager.createDao(cs, (Class<THIS>) this.getClass());
+    }
+}
